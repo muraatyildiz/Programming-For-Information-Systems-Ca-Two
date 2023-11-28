0b93759f35c08e262220a59fdcccd2427da1ec97
murat yıldız <mrtyldz118@gmail.com>
2023-11-28 21:05:39 +0000

    Connected to remote database

--- WebShop/API/dbconn.py
+++ WebShop/API/dbconn.py
@@ -1,19 +1,26 @@
-import sqlalchemy as sa
-from sqlalchemy import create_engine
-import urllib
-
-conn_str = urllib.parse.quote_plus(
-    'Data Source Name=KNITNETDS;'
-    'Driver={SQL Server};'
-    'Server=(localdb)\MSSQLLocalDB;' 
-    'Database=;AdvancedDBFRI'
-    'Trusted_connection=yes;'
-)
- 
+from sshtunnel import SSHTunnelForwarder
+import mysql.connector
 try:
-    
-    ApiSQLEngine = create_engine('mssql+pyodbc:///?odbc_connect={}'.format(conn_str))
-    print("Passed")
- 
-except:
-    print("failed!")
+    server = SSHTunnelForwarder(('20.218.157.149',22),
+                                ssh_username='murat',
+                                ssh_password="M2001695720016957.z",
+                                ssh_proxy_enabled= True,
+                                remote_bind_address=('127.0.0.1', 3306)
+                                )
+    server.start()
+    print("Server started")
+    mysql = mysql.connector.connect(user='web', password='webPass',
+                                    host='127.0.0.1',
+                                    database='student',
+                                    port=server.local_bind_port,
+                                    
+                                    )
+    if mysql!=None:
+        print("connect",mysql)
+    else:
+        print("error", mysql)          
+except BaseException as e:
+    print('Problem with', e)
+finally:
+    if server:
+        server.close   
