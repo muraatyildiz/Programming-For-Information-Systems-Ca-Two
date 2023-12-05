bc86f2614fe4c65a6ee8552540adcd244f84d16a
irenesengo <irenesengo@gmail.com>
2023-12-05 12:14:41 +0000

    updated API code

--- WebShop/UI/store/index.js
+++ WebShop/UI/store/index.js
@@ -16,3 +16,37 @@ export const mutations = {
 		}
 	}
 };
+export const actions = {
+	requestPost(vuexContext, send) {	
+		send.link = vuexContext.state.ApiLink + send.link;
+        console.log(send.data)
+		return Vue.http.post(send.link,  send.data).catch((r) => {			
+		});
+	},
+	requestGet(vuexContext, link) {
+	   link = vuexContext.state.ApiLink + link;
+		return Vue.http.get(link).catch((r) => {
+					});
+	},
+    requestPut(vuexContext, send) {	
+		send.link = vuexContext.state.ApiLink + send.link;
+        console.log(send.data)
+		return Vue.http.put(send.link, send.data).catch((r) => {			
+		});
+	},
+	requestDelete(vuexContext, link) {
+	   link = vuexContext.state.ApiLink + link;
+       console.log(link);
+		return Vue.http.delete(link).catch((r) => {
+		});
+	},
+};
+
+export const getters = {
+	getUserInfo(state) {
+		if (state.userInfo) {
+			return state.userInfo;
+		}
+		return null;
+	}
+};
