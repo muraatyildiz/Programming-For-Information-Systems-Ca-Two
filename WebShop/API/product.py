91a7f165870c0635b9161a8a3cdb010db8661fb6
irenesengo <irenesengo@gmail.com>
2023-12-05 11:47:18 +0000

    updated product.py

--- WebShop/API/product.py
+++ WebShop/API/product.py
@@ -33,20 +33,26 @@ CORS(product)
 
 @product_api.route("/test")#URL leading to method
 def test(): # Name of the method
- return("Hello World!<BR/>THIS IS ANOTHER TEST!") #indent this line
+ return("Hello World!<BR/>THIS IS ANOTHER TEST!") 
 
 @product_api.route("/yest")#URL leading to method
 def yest(): # Name of the method
- return("Hello World!<BR/>THIS IS YET ANOTHER TEST!") #indent this line
+ return("Hello World!<BR/>THIS IS YET ANOTHER TEST!") 
 
-@product_api.route("/add", methods=['GET', 'POST']) #Add Student
+@product_api.route("/add", methods=['GET', 'POST']) #Add product
 def add():
   if request.method == 'POST':
+    print("here",request.form)
     name = request.form['name']
-    email = request.form['email']
-    print(name,email)
+    category = request.form['category']
+    price = request.form['price']
+    stock = request.form['stock']
+    imgUrl = request.form['imgUrl']
+    description = request.form['description']
+    
     cur = mysql.cursor() #create a connection to the SQL instance
-    s='''INSERT INTO students(studentName, email) VALUES('{}','{}');'''.format(name,email)
+    s='''INSERT INTO Product7 (Name, Category,Price,Stock,ImgURL,Description) VALUES('{}','{}','{}','{}','{}','{}');'''.format(name,category,price,stock,imgUrl,description)
+    print(s)
     product.logger.info(s)
     cur.execute(s)
     mysql.commit()
@@ -54,17 +60,40 @@ def add():
     return render_template('add.html')
 
   return '{"Result":"Success"}'
+@product_api.route("/update", methods=['GET', 'POST']) #Add product
+def update():
+  if request.method == 'POST':   
+    id = request.form['id']
+    name = request.form['name']
+    category = request.form['category']
+    price = request.form['price']
+    stock = request.form['stock']
+    imgUrl = request.form['imgUrl']
+    description = request.form['description']
+    cur = mysql.cursor() #create a connection to the SQL instance
+    s='''UPDATE Product7 SET Name ='{}' , Category='{}',Price='{}',Stock='{}',ImgURL='{}',Description='{}'  WHERE Product_ID ='{}';'''.format(name,category,price,stock,imgUrl,description,id)
+    print(s)
+    product.logger.info(s)
+    cur.execute(s)
+    mysql.commit()
+  else:
+    return render_template('add.html')
+  return '{"Result":"Success"}'
 @product_api.route("/list") #Default - Show Data
 def hello(): # Name of the method
   cur = mysql.cursor() #create a connection to the SQL instance
-  cur.execute('''SELECT * FROM students''') # execute an SQL statment
+  cur.execute('''SELECT * FROM Product7''') # execute an SQL statment
   rv = cur.fetchall() #Retreive all rows returend by the SQL statment
   Results=[]
   for row in rv: #Format the Output Results and add to return string
     Result={}
-    Result['Name']=row[0].replace('\n',' ')
-    Result['Email']=row[1]
-    Result['ID']=row[2]
+    Result['id']=row[0]
+    Result['name']=row[1]
+    Result['category']=row[2]
+    Result['stock']=row[3]
+    Result['price']=row[4]
+    Result['imgUrl']=row[5]
+    Result['description']=row[6]
     Results.append(Result)
   response={'Results':Results, 'count':len(Results)}
   ret=product.response_class(
@@ -73,3 +102,35 @@ def hello(): # Name of the method
     mimetype='application/json'
   )
   return ret #Return the data in a string format
+@product_api.route('/delete/<int:item_id>', methods=['DELETE'])
+def delete_items(item_id):
+ try:
+        cur = mysql.cursor()
+        cur.execute('DELETE FROM Product7 WHERE Product_ID = %s', (item_id,))
+        mysql.commit()
+        cur.close()       
+        
+        response = {
+            'error' : False,
+            'message': 'Product Deleted Successfully',
+            'data': { 'item_id': item_id }
+        }
+        ret=product.response_class(
+        response=json.dumps(response),
+        status=200,
+        mimetype='application/json'
+           )
+        return ret
+ except Exception as e:
+        # Handle any exceptions that may occur during the process
+        response = {
+            'error' : False,
+            'message': f'Error Occurred: {e}',
+            'data': None         
+        }
+        ret=product.response_class(
+        response=json.dumps(response),
+        status=500,
+        mimetype='application/json'
+           )
+        return ret
