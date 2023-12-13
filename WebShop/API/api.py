c7e0181e7c9f70cbda4ee84dfdd9745948fd88da
murat yıldız <mrtyldz118@gmail.com>
2023-12-13 12:43:19 +0000

    Updated api navigation

--- WebShop/API/api.py
+++ WebShop/API/api.py
@@ -10,15 +10,17 @@ from dbconn import mysql
 from flask import Blueprint
 from product import product_api
 from fileUpload import fileUpload_api
-
+from order import order_api
+from login import login_api
 
 
 app = Flask(__name__)
 
-# app.register_blueprint()
-# app.register_blueprint(fileUpload_api)
+
 app.register_blueprint(product_api, url_prefix='/product/')
 app.register_blueprint(fileUpload_api, url_prefix='/file/')
+app.register_blueprint(order_api, url_prefix='/order/')
+app.register_blueprint(login_api, url_prefix='/login/')
 CORS(app)
 
 
